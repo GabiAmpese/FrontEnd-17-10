518827813b99bdef093daa6f24359d7686257a91
Gabriela Ampese <ampesegabriela@gmail.com>
2025-10-18 00:42:59 +0000

    Alterações da aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+import {TodoList} from './components/TodoList'
+
 function App() {
+  const [isLogged, setIsLogged] = useState(false)
+
+  const doLogin = () => setIsLogged(true)
+  const undoLogin = () => setIsLogged(false)
+
   return (
-    <h1 class="text-3xl font-bold underline text-red-700">
-      Hello world!
-    </h1>
+    <>
+    {isLogged ? <div>Seja Bem Vindo, Fulano de tal! <a href="#" onClick={undoLogin}>Deslogue aqui</a></div> : <div>Faça seu login, <a href="#" onClick={doLogin}>clique aqui</a>!</div>}
+    <TodoList/>
+    </>
   )
 }
 
